09f3c187ae4926a9da1dab86f4f95560b295d8e9
ysmn-a <yasmine@YasmineASUS.localdomain>
2026-03-06 13:06:01 +0100

    Fix

--- refonte-dashboard/includes/learnworlds.class.php
+++ refonte-dashboard/includes/learnworlds.class.php
@@ -45,6 +45,7 @@ class LearnWorlds
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
         curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
         curl_setopt($ch, CURLOPT_HEADER, true);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
     }
 
     // ---------------------------------------------------------------
